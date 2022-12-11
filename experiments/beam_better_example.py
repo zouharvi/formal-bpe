417fe368a1890702123dbee957ae09d7cfabf1c3
Vilém Zouhar <vilem.zouhar@gmail.com>
2022-12-11 17:32:58 +0100

    add exact brute algo

--- experiments/beam_better_example.py
+++ experiments/beam_better_example.py
@@ -1,60 +1,80 @@
 #!/usr/bin/env python3
 
-from collections import Counter
+import orderedset
 import itertools
 import string
 import argparse
-import orderedset
 from arsenal.iterextras import take
 from faster_bpe.model_slow_beam import SlowBeamBPE
+from faster_bpe.model_exact_brute import ExactBruteBPE
 from faster_bpe.model_slow import SlowBPE
-from faster_bpe.utils import pairs_in_list, flat_seq
 from rich.progress import track
 
 args = argparse.ArgumentParser()
+args.add_argument("--example-length-start", type=int, default=None)
 args.add_argument("--example-length", type=int, default=12)
-args.add_argument("--alphabet-size", type=int, default=4)
+args.add_argument("--alphabet-size", type=int, default=3)
 args.add_argument("--beam-size", type=int, default=5)
-args.add_argument("--merge-count", type=int, default=3)
+args.add_argument("--merge-count", type=int, default=2)
 args = args.parse_args()
 
-iterator = map("".join, itertools.product(string.ascii_lowercase[:args.alphabet_size], repeat=args.example_length))
-total = args.alphabet_size**args.example_length
+if args.example_length_start is None:
+    args.example_length_start = args.example_length
 
-for example in map(
-    ''.join,
-    track(iterator, total=total)
-):
-    bpe_greedy = SlowBPE(fix_overlap=True)
-    result_greedy, indecision = bpe_greedy.fit_greedy(
-        example, T=args.merge_count,
-        debug_output=True, indecision_output=True
+min_ratio = 1
+alphabet = string.ascii_lowercase[:args.alphabet_size]
+
+for length in range(args.example_length_start, args.example_length + 1):
+    iterator = map(
+        "".join,
+        itertools.product(alphabet, repeat=length)
     )
-    result_greedy = result_greedy[0]
+    total = args.alphabet_size**length
+    print(f"\n{length}\n")
+    for example in map(
+        ''.join,
+        track(iterator, total=total)
+    ):
 
+        # the letters must be sorted otherwise discard
+        # this saves args.alphabet_size times the work
+        example_letters = list(orderedset.OrderedSet(example))
+        if not all(example_letters[i] <= example_letters[i + 1] for i in range(len(example_letters) - 1)):
+            continue
+        # the first n letters must be present to prevent duplicates like b,c,d,e..
+        if not set(example_letters).issubset(alphabet):
+            continue
 
-    bpe_beam = SlowBeamBPE()
-    result_beam = bpe_beam.fit_beam_search(
-        example, T=args.merge_count,
-        B=args.beam_size
-    )
+        model = SlowBPE(fix_overlap=True)
+        result_greedy, indecision = model.fit_greedy(
+            example, T=args.merge_count,
+            debug_output=True, indecision_output=True
+        )
+        result_greedy = result_greedy[0]
 
-    if len(result_greedy) > len(result_beam):
-        print("Example:    ", example, "indecision", indecision)
-        print(f"Greedy:      ({len(result_greedy)})", result_greedy)
-        print(f"Beam search: ({len(result_beam)})", result_beam)
-        print("====")
+        # model = ExactBruteBPE(fix_overlap=True)
+        # result_exact = model.fit_greedy(
+        #     example, T=args.merge_count,
+        # )
 
-# print(len(result_greedy), len(result_beam))
+        bpe_beam = SlowBeamBPE()
+        result_beam = bpe_beam.fit_beam_search(
+            example, T=args.merge_count,
+            B=args.beam_size
+        )
 
+        if min_ratio >= len(result_beam) / len(result_greedy) and min_ratio != 1:
+            # if len(result_greedy) > len(result_beam):
+            print("Example:    ", example, "indecision", indecision)
+            print(f"Greedy:      ({len(result_greedy)})", result_greedy)
+            print(f"Beam search: ({len(result_beam)})", result_beam)
+            print(min_ratio)
+            print("====")
+        min_ratio = min(min_ratio, len(result_beam) / len(result_greedy))
+        # assert len(result_exact) <= len(result_greedy)
 
-# # the letters must be sorted otherwise discard
-# # this saves args.alphabet_size times the work
-# example_letters = list(orderedset.OrderedSet(example))
-# if not all(example_letters[i] <= example_letters[i+1] for i in range(len(example_letters) - 1)):
-#     continue
-
-# # discard examples which don't use all letters
-# if len(example_letters) != args.alphabet_size:
-#     continue
-# # print(example, example_letters)
+        # if len(result_exact) < len(result_greedy) and not indecision:
+        #     print("Example:    ", example, "indecision", indecision)
+        #     print(f"Greedy:      ({len(result_greedy)})", result_greedy)
+        #     print(f"Beam search: ({len(result_beam)})", result_beam)
+        #     print("====")
---
+++ faster_bpe/model_exact_brute.py
@@ -0,0 +1,74 @@
+from collections import defaultdict
+from faster_bpe.utils import pairs_in_list, flat_seq, debug_flat_seq
+from typing import Dict, List, Tuple
+from rich.progress import track
+
+class ExactBruteBPE:
+    def __init__(self, fix_overlap=False):
+        self.fix_overlap = fix_overlap
+        if fix_overlap:
+            self.get_word_pair_counts = self.get_word_pair_counts_fix_overlap
+
+    @staticmethod
+    def apply_merge_slow(token, pair):
+        ys_word = []
+        i = 0
+        N = len(token)
+        while i < N:
+            if i < N - 1 and (token[i], token[i + 1]) == pair:
+                ys_word.append(pair)
+                i += 2
+            else:
+                ys_word.append(token[i])
+                i += 1
+        return ys_word
+
+    @staticmethod
+    def get_word_pair_counts(tokens_freqs):
+        pairs = defaultdict(int)
+        for token, token_freq in tokens_freqs.values():
+            for (x, y) in pairs_in_list(token):
+                pairs[x, y] += token_freq
+
+        return pairs
+
+    @staticmethod
+    def get_word_pair_counts_fix_overlap(tokens):
+        pairs = defaultdict(int)
+        prev_pair = None
+        for (x, y) in pairs_in_list(tokens):
+            # increment only if the prev suffix does not match our prefix
+            # otherwise wrong estimate on `aaa`
+            if (x,y) != prev_pair:
+                pairs[x, y] += 1
+                prev_pair = (x, y)
+            else:
+                # make sure to clear it so that for `aaaa` we count it twice
+                prev_pair = None
+
+        return pairs
+
+    @staticmethod
+    def top_pair(pairs):
+        return max(pairs, key=pairs.__getitem__)
+
+    def fit_greedy(self, tokens, T):
+        if T == 0:
+            return [debug_flat_seq(x) for x in tokens]
+
+        outputs = []
+        self.model = ExactBruteBPE(self.fix_overlap)
+
+        pairs = self.get_word_pair_counts(tokens)
+        # if len(pairs) == 0: ???
+        for pair, pair_freq in pairs.items():
+            # TODO: resolve the other indecision
+            # UPDATE: that one is implicitly fixed by swapping the operands
+            tokens_new = self.apply_merge_slow(tokens, pair)
+            outputs.append(self.model.fit_greedy(tokens_new, T-1))
+
+        # this mutates tokens_freqs
+
+        output = min(outputs, key=len)
+        output = [debug_flat_seq(x) for x in output]
+        return output
