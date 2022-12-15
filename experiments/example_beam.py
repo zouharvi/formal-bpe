4ef3af4f74ae6fc5356376b87db96c54a258ff03
Vilém Zouhar <vilem.zouhar@gmail.com>
2022-12-15 13:03:33 +0100

    add slow exact insert bpe

--- experiments/example_beam.py
+++ experiments/example_beam.py
@@ -6,16 +6,17 @@ import string
 import argparse
 from arsenal.iterextras import take
 from faster_bpe.model_slow_beam import SlowBeamBPE
+from faster_bpe.model_slow_insert import SlowInsertBPE
 from faster_bpe.model_exact_brute import ExactBruteBPE
 from faster_bpe.model_slow import SlowBPE
 from rich.progress import track
 
 args = argparse.ArgumentParser()
 args.add_argument("--example-length-start", type=int, default=None)
-args.add_argument("--example-length", type=int, default=12)
-args.add_argument("--alphabet-size", type=int, default=3)
+args.add_argument("--example-length", type=int, default=8)
+args.add_argument("--alphabet-size", type=int, default=2)
 args.add_argument("--beam-size", type=int, default=5)
-args.add_argument("--merge-count", type=int, default=2)
+args.add_argument("--merge-count", type=int, default=4)
 args = args.parse_args()
 
 if args.example_length_start is None:
@@ -42,7 +43,7 @@ for length in range(args.example_length_start, args.example_length + 1):
         if not all(example_letters[i] <= example_letters[i + 1] for i in range(len(example_letters) - 1)):
             continue
         # the first n letters must be present to prevent duplicates like b,c,d,e..
-        if not set(example_letters).issubset(alphabet):
+        if not set(example_letters).issubset(alphabet[:len(set(example_letters))]):
             continue
 
         model = SlowBPE(fix_overlap=True)
@@ -63,14 +64,18 @@ for length in range(args.example_length_start, args.example_length + 1):
             B=args.beam_size
         )
 
-        if min_ratio >= len(result_beam) / len(result_greedy) and min_ratio != 1:
-            # if len(result_greedy) > len(result_beam):
+        n_beam = len(example) - len(result_beam)
+        n_greedy = len(example) - len(result_greedy)
+        # print(f"Ratio: {n_greedy/ n_beam:.2f}")
+
+        if min_ratio >= n_greedy / n_beam and n_beam / n_greedy != 1:            
+            min_ratio = n_greedy/n_beam
             print("Example:    ", example, "indecision", indecision)
-            print(f"Greedy:      ({len(result_greedy)})", result_greedy)
-            print(f"Beam search: ({len(result_beam)})", result_beam)
-            print(min_ratio)
+            print(f"Greedy:      ({n_greedy})", result_greedy)
+            print(f"Beam search: ({n_beam})", result_beam)
+            print(f"Ratio:       {n_greedy/ n_beam:.2f}")
             print("====")
-        min_ratio = min(min_ratio, len(result_beam) / len(result_greedy))
+
         # assert len(result_exact) <= len(result_greedy)
 
         # if len(result_exact) < len(result_greedy) and not indecision:
---
+++ faster_bpe/model_slow_insert.py
@@ -0,0 +1,105 @@
+from collections import defaultdict
+from faster_bpe.utils import pairs_in_list, flat_seq, debug_flat_seq
+from typing import Dict, List, Tuple
+from rich.progress import track
+
+
+class SlowInsertBPE:
+    def __init__(self, fix_overlap=False, tokenize=False):
+        if fix_overlap:
+            self.get_word_pair_counts = self.get_word_pair_counts_fix_overlap
+
+        self.tokenize = tokenize
+
+    @staticmethod
+    def apply_merge_slow(token, pair):
+        ys_word = []
+        i = 0
+        N = len(token)
+        while i < N:
+            if i < N - 1 and (token[i], token[i + 1]) == pair:
+                ys_word.append(pair)
+                i += 2
+            else:
+                ys_word.append(token[i])
+                i += 1
+        return ys_word
+
+    @staticmethod
+    def encode(token, pairs):
+        for pair in pairs:
+            token = SlowInsertBPE.apply_merge_slow(token, pair)
+        return token
+
+    @staticmethod
+    def get_word_pair_counts(tokens_freqs):
+        raise NotImplementedError
+        pairs = defaultdict(int)
+        for token, token_freq in tokens_freqs.values():
+            for (x, y) in pairs_in_list(token):
+                pairs[x, y] += token_freq
+
+        return pairs
+
+    @staticmethod
+    def get_word_pair_counts_fix_overlap(token):
+        pairs = defaultdict(int)
+        prev_pair = None
+        for (x, y) in pairs_in_list(token):
+            # increment only if the prev suffix does not match our prefix
+            # otherwise wrong estimate on `aaa`
+            if (x, y) != prev_pair:
+                pairs[x, y] += 1
+                prev_pair = (x, y)
+            else:
+                # make sure to clear it so that for `aaaa` we count it twice
+                prev_pair = None
+
+        return pairs
+
+    def insert_best(self, merge_pairs, pair, token):
+        # only for comparison, is not actually an edge case
+        base_length = len(self.encode(token, merge_pairs+[pair]))
+        best_merge_pairs = merge_pairs+ [pair]
+
+        for i in range(len(merge_pairs)):
+            new_merge_pairs = merge_pairs[:i] + [pair]+merge_pairs[i:]
+            new_length = len(self.encode(token, new_merge_pairs))
+            if new_length < base_length:
+                print("HIT")
+                best_merge_pairs = new_merge_pairs
+
+        return best_merge_pairs, self.encode(token, best_merge_pairs)
+
+    @staticmethod
+    def top_pair(pairs):
+        return max(pairs, key=pairs.__getitem__)
+
+    def fit_greedy(self, tokens, T, debug_output=False, progress_bar=False, indecision_output=False):
+        indecision = False
+        merge_pairs = []
+        tokens_raw = list(tokens)
+
+        iterator = track(range(T)) if progress_bar else range(T)
+        for t in iterator:
+            pairs = self.get_word_pair_counts(tokens)
+            if len(pairs) == 0:
+                break
+
+            pair = self.top_pair(pairs)
+
+            merge_pairs, tokens = self.insert_best(merge_pairs, pair, tokens_raw)
+
+        tokens = self.encode(tokens, merge_pairs)
+
+        if debug_output:
+            flattener = debug_flat_seq
+        else:
+            flattener = flat_seq
+
+        output = [flattener(x) for x in tokens]
+
+        if indecision_output:
+            return output, indecision
+        else:
+            return output
