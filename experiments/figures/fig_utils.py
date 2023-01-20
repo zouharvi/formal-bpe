599989255572aab3c2f70aaff5208b462966a182
Vilém Zouhar <vilem.zouhar@gmail.com>
2023-01-20 14:52:59 +0100

    redo figures in CMU serif font

--- experiments/figures/fig_utils.py
+++ experiments/figures/fig_utils.py
@@ -17,5 +17,7 @@ mpl.rcParams['axes.prop_cycle'] = cycler(color=COLORS)
 mpl.rcParams['lines.linewidth'] = 2
 mpl.rcParams['lines.markersize'] = 7
 mpl.rcParams['axes.linewidth'] = 1.5
+mpl.rc('text', usetex=True)
+mpl.rcParams['font.family'] = "serif"
 
 COLORS_EXTRA = ["#9c2963", "#fb9e07"]
